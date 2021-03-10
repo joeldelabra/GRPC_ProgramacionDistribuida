ea1cd048d770fda2bbb6e09204fa88df8d06d9c4
joeldelabra <joeldelabra@gmail.com>
2021-03-10 02:20:10 -0700

    GRPC v2

--- cliente-grpc.js
+++ cliente-grpc.js
@@ -8,24 +8,27 @@ const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || 'localhost:4000';
 
-const cliente = new receta.ServicioReceta(TARGET,
-    grpc.credentials.createInsecure());
+const cliente = new receta.ServicioReceta(TARGET, grpc.credentials.createInsecure());
 
-    const obtenerMetaData = util.promisify(cliente.obtenerMetaData.bind(cliente));
-    const obtenerReceta = util.promisify.obtenerReceta.bind(cliente);
+const obtenerMetaData = util.promisify(cliente.obtenerMetaData.bind(cliente));
+const obtenerReceta = util.promisify(cliente.obtenerReceta.bind(cliente));
 
-    server.get('/', async () => {
-        const[meta,receta] = await Promise.all(
-            obtenerMetaData({}), 
-            obtenerReceta({id:42})
-        );
-        return {
-            pid_cliente : process.pid,
-            metadata_server : meta,
-            receta
-        }
-     });
+server.get('/', async () => {
 
-     server.listen(PORT,HOST, () => {
-         console.log(`Cliente escuchando en: ${HOST}: ${PORT}/`)
-     });
+    const [meta, receta] = await Promise.all(
+        obtenerMetaData({}),
+        obtenerReceta({id: 42})
+    );
+    return{
+        pid_cliente : process.pid,
+        metadata_server: meta, 
+        receta
+    }
+
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Cliente escuchando en: ${HOST}:${PORT}/`)
+});
+
+
